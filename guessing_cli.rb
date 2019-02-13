741cfb7c4c803760f32a28351d7c4a368c8a6703
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-13 16:12:08 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,4 +1,11 @@
 # Code your solution here!
 def run_guessing_game
 
+
+  if guess == random
+    puts "You guessed the correct number!"
+  else
+    puts "The computer guessed #{random}."
+  end
+  
 end
