c054d4eec2503506b02e5293d4ed7bf8fcfa0f70
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-13 16:15:13 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -5,6 +5,8 @@ def run_guessing_game
   random = rand(1..6)
   if guess == random
     puts "You guessed the correct number!"
+  elsif guess == "exit"
+    break
   else
     puts "The computer guessed #{random}."
   end
